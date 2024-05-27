d097d30a134102dfe5dddbf6743b8ba02399e6b4
ahmad96_hijazi <ahmad96.hijazi@gmail.com>
2024-05-27 12:56:00 +0300

    working

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 import { createApp } from 'vue'
 
 import App from './App.vue'
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import axios from "axios";
 import VueAxios from "vue-axios";
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
-const app= createApp(App)
+const app = createApp(App)
 
 const options = {
   confirmButtonColor: '#41b882',
